491fed190f6efed2e9cfb17e4f89def3ddc3a9c3
chen po hung <ski94114@skinsurance.com.tw>
2022-11-24 14:33:30 +0800

    update

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -1,8 +1,19 @@
 import Item from '../Item.vue'
+import { createApp } from 'vue'
 
 describe('Item.vue', ()=>{
    test('hung Item', ()=>{
     expect(true).toBe(true)
     console.log(Item)
+
+    //check document Content
+    const el = document.createElement('div')
+    el.id = 'app'
+    document.body.appendChild(el)
+    const app = createApp(Item).mount('#app')
+
+    console.log(document.body.outerHTML)
+
+    expect(app.$el.textContent).toContain('item')
     }) 
 })
